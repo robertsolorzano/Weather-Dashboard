f61a42ff2eeaf8fe30c012c86058344378d44ab4
Robert Solorzano <robertsolorzano510@gmail.com>
2024-01-26 13:01:21 -0800

    built getWeatherdata api request, console logged thr data and seeing a response, wrapped event listnerer in the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,38 @@
-//ideally I want to add my api key to a .env and install dotenv if this was an express app for protection and to not break any policy agreements
+//Weather Dashaboard
+
+document.addEventListener('DOMContentLoaded', function () {
+document.getElementById('searchButton').addEventListener('click', function() {
+    const city = document.getElementById('cityInput').value.trim();
+    if (city) {
+        getWeatherData(city); // Make sure this function is defined
+    } else {
+        alert('Please enter a city name');
+    }
+    });
+
+});
+
+
 const apiKey = '8f71d08fa9cb2980da4905c964414ed3';
 
-document.getElementById('searchButton').addEventListener('click', function() {
-    const city = document.getElementById('cityInput').ariaValueMax.trim();
-    if (city) {
-        getWeatherData(city);//need to make this function later
-    } else {
-        alert('PLease enter a city name');
-    }
-})
+function getWeatherData(city) {
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+
+    fetch(currentWeatherUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data:', data)
+            displayCurrentWeather(data); //need to make this later
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+
+}
+
+
